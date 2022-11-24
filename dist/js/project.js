635ee5f80c8c365ec0feca5be487d6647745fff8
Falzz1010 <setiawandandy845@gmail.com>
2022-11-24 10:07:55 +0700

    first commit

---
+++ dist/js/project.js
@@ -0,0 +1,9 @@
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  const windowposition = window.scrollY > 0;
+  navBar.classList.toggle
+  ("scrolling-active", windowposition);
+
+
+});
